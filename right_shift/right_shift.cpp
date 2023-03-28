5e7a4284bd6ea98b27d19fe3969b7211747e8109
shanmukh <s.shanmukh8july@gmail.com>
2023-03-28 15:38:53 +0530

    right shift

---
+++ right_shift/right_shift.cpp
@@ -0,0 +1,45 @@
+#include <systemc.h>
+#include <iomanip>
+#include "const.h"
+
+SC_MODULE(right_shift_N) {
+	sc_in<sc_bv<N>> inp;
+	sc_in<sc_bv<4>> shift_by;
+	sc_out<sc_bv<N>> out;
+	sc_in<bool> clk;
+
+	void right_shift_func(){
+		sc_bv<N> inp0;
+		sc_bv<4> shift0;
+		int i, j;
+
+		shift0 = shift_by.read();
+
+		if(shift0 == 0b0){
+			out.write(inp.read());
+		}
+		else {
+			inp0 = inp.read();
+			
+			sc_uint<4> s;
+			s = shift0;
+			for(i = 0; i < s; i++){
+				for(j = 0; j < N; j++){
+					if(j!=15){
+						inp0[j] = inp0[j+1];
+					}
+					else {
+						inp0[j] = 0;
+					}
+				}
+			}
+			out.write(inp0);
+		}			
+	}
+
+	SC_CTOR(right_shift_N){
+		SC_METHOD(right_shift_func);
+		sensitive << clk.pos();
+		sensitive << inp << shift_by;
+	}
+};
---
+++ right_shift/right_shift_test.cpp
@@ -0,0 +1,56 @@
+#include "right_shift.cpp"
+
+
+
+SC_MODULE(right_shift_test){
+	sc_signal<sc_bv<N>> inp;
+	sc_signal<sc_bv<4>> shift_by;
+	sc_signal<sc_bv<N>> out;
+	sc_clock clk;
+
+	sc_trace_file *trace;
+
+	right_shift_N shift_r;
+
+	void stimulus();
+
+	SC_CTOR(right_shift_test):
+		shift_r("shift_r"),
+		clk("clk", tp)
+	{
+		shift_r.inp(inp);
+		shift_r.shift_by(shift_by);
+		shift_r.out(out);
+		shift_r.clk(clk);
+
+		trace = sc_create_vcd_trace_file("right_shift");
+		sc_trace(trace, inp, "input");
+		sc_trace(trace, shift_by, "shift_by");
+		sc_trace(trace, out, "output");
+		sc_trace(trace, clk, "clk");
+		
+		SC_THREAD(stimulus);
+	}
+
+	~right_shift_test(){
+		sc_close_vcd_trace_file(trace);
+	}
+};
+
+void right_shift_test::stimulus(){
+	srand(time(NULL));
+	while(true){
+		inp.write(rand() % (1 << N));
+		shift_by.write(rand() % (1 << 4));
+		cout << "input = " << inp.read() << " shift_r_by = " << shift_by.read() << " output = " << out.read() << endl;
+		wait(tp);
+	}
+}
+
+
+int sc_main(int argc, char* argv[]){
+	right_shift_test test("test");
+	sc_start(15*tp);
+	sc_stop();
+	return 0;
+}
